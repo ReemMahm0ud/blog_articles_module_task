23a3d1cde613ef7c06d1c80fede18a255908ea40
ReemMahm0ud <reemhashad82@gmail.com>
2021-12-05 22:26:13 +0200

    add API routes

--- Modules/Article/Routes/api.php
+++ Modules/Article/Routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
+use Modules\Article\Http\Controllers\API\ArticleAPIController;
+
+use Illuminate\Routing\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,18 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/article', function (Request $request) {
     return $request->user();
 });
+
+//post article
+Route::post('/addArticle',[ArticleAPIController::class, 'store'] );
+
+// get all articles
+Route::get('/getALLarticles',[ArticleAPIController::class, 'index'] );
+
+//get article by id
+Route::get('/articleByID',[ArticleAPIController::class, 'show'] );
+
+//update article
+Route::put('/updateArticle',[ArticleAPIController::class, 'update'] );
+
+//delete article
+Route::delete('/deleteArticle',[ArticleAPIController::class, 'destroy'] );
